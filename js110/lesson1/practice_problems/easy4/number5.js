1504330a7fb488b3269038229d2fc38b30425a59
rgharding <richardharding305@gmail.com>
2023-03-18 01:24:56 -0500

    js110 lesson1 small problems easy4 number5 additional solution added

--- js110/lesson1/practice_problems/easy4/number5.js
+++ js110/lesson1/practice_problems/easy4/number5.js
@@ -62,3 +62,21 @@ console.log(runningTotal([2, 5, 13])); // [2, 7, 20]
 console.log(runningTotal([14, 11, 7, 15, 20])); // [14, 25, 32, 47, 67]
 console.log(runningTotal([3])); // [3]
 console.log(runningTotal([])); // []
+
+
+
+function runningTotal2(inputArray) {
+  let outputArray = [];
+  let sum = 0;
+
+  for (let i = 0; i < inputArray.length; i += 1) {
+    outputArray[i] = sum += inputArray[i];
+  }
+
+  return outputArray;
+}
+
+console.log(runningTotal2([2, 5, 13])); // [2, 7, 20]
+console.log(runningTotal2([14, 11, 7, 15, 20])); // [14, 25, 32, 47, 67]
+console.log(runningTotal2([3])); // [3]
+console.log(runningTotal2([])); // []

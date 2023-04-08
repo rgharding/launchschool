fce00ab23c23e9d6c4e821362b1b82ef12d0669d
rgharding <richardharding305@gmail.com>
2023-04-08 15:05:54 -0500

    js101 Lesson 2 calculator - Basic Calculator Completed

--- js101/lesson2/calculator.js
+++ js101/lesson2/calculator.js
@@ -1,1 +1,35 @@
-// Welcome To Rich's Calculator
+let sync = require('readline-sync');
+
+console.log('Welcome to Calculator!!!');
+
+console.log('Enter First Number: ');
+let firstNumber = Number(sync.question());
+
+console.log('Enter Second Number');
+let secondNumber = Number(sync.question());
+
+console.log('Enter Operation: \n1: Add\n2: Subtract \n3: Multiply \n4: Divide' );
+let operation = Number(sync.question());
+
+let result;
+switch (operation) {
+   
+
+  case 1:
+    result = firstNumber + secondNumber;
+    break;
+  
+  case 2:
+    result = firstNumber - secondNumber;
+    break;
+  
+  case 3:
+    result = firstNumber * secondNumber; 
+    break;
+
+  case 4: 
+    result = firstNumber / secondNumber;
+    break;
+}
+
+console.log(`The result is: ${result}`);

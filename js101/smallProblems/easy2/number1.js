9131e32b70469a41dd5ca706484f39e3859aa507
rgharding <richardharding305@gmail.com>
2023-04-09 21:57:15 -0500

    js 101 small problems easy2 numbers 1 & 2

--- js101/smallProblems/easy2/number1.js
+++ js101/smallProblems/easy2/number1.js
@@ -1,10 +1,10 @@
 function greetings(arr, obj) {
 	let stringFromArray = arr.join(' ');
   let keysFromObj = Object.keys(obj);
-  return `Hello, ${stringFromArray}! Nice to hava a ${obj[keysFromObj[0]]} ${obj[keysFromObj[1]]} around.`
+  return `Hello, ${stringFromArray}! Nice to hava a ${obj[keysFromObj[0]]} ${obj[keysFromObj[1]]} around!`
 }
 
 console.log(
   greetings(["John", "Q", "Doe"], { title: "Master", occupation: "Plumber" })
 );
-// logs Hello, John Q Doe! Nice to have a Master Plumber around.
+// logs Hello, John Q Doe! Nice to have a Master Plumber around!

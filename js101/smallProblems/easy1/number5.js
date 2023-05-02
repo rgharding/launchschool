37da3b99222e24c084651cc8dc9f4b04d54131e0
rgharding <richardharding305@gmail.com>
2023-05-02 08:10:11 -0500

    js101 small problems easy1 #5

--- js101/smallProblems/easy1/number5.js
+++ js101/smallProblems/easy1/number5.js
@@ -1,1 +1,10 @@
 // This will be a simple tip calculator
+let billAmount = Number.parseFloat((prompt('Enter Bill Amount')));
+let tipPercent = Number(prompt('Enter tip percentage'));
+let tipDecimal = tipPercent / 100;
+
+let tipAmount = (tipDecimal * billAmount).toFixed(2);
+let total = (tipAmount + billAmount).toFixed(2);
+
+
+console.log(`bill: ${billAmount}, tipPercent: ${tipPercent}, Tip Amount: ${tipAmount}, Total: ${total}`);   

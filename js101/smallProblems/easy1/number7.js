cd3d5e36f341ec2a68a84a1ecf0efc2c85c5c60e
rgharding <richardharding305@gmail.com>
2023-05-10 09:10:19 -0500

    js101 small problems easy1 #7 completed

--- js101/smallProblems/easy1/number7.js
+++ js101/smallProblems/easy1/number7.js
@@ -1,1 +1,20 @@
-// Complete
+function shortLongShort(str1, str2) {
+  let longer;
+  let shorter;
+
+  if (str1.length > str2.length) {
+    longer = str1;
+    shorter = str2;
+  } else {
+    longer = str2;
+    shorter = str1;
+  }
+  return shorter + longer + shorter;
+}
+
+
+
+
+console.log(shortLongShort('abc', 'defgh'));    // "abcdefghabc"
+console.log(shortLongShort('abcde', 'fgh'));   // "fghabcdefgh"
+console.log(shortLongShort('', 'xyz'));         // "xyz"

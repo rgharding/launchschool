80b25559aedd0bd018ddf04d0db4edbf09b6bd7a
rgharding <richardharding305@gmail.com>
2023-05-17 06:48:48 -0500

    js101 small problems easy2 #1

---
+++ js101/smallProblems/easy2/number1.js
@@ -0,0 +1,17 @@
+function greetings(arr, obj) {
+  let name = arr.join(' ');
+  let title = obj.title;
+  let occupation = obj.occupation;
+
+  return `Hello, ${name}! Nice to have a ${title} ${occupation} joining us!`
+}
+
+
+let personArr = ['Bob', 'R',  'Smith'];
+
+let personObj = {
+  title: "Journeyman",
+  occupation: 'Electrician',
+}
+
+console.log(greetings(personArr, personObj));

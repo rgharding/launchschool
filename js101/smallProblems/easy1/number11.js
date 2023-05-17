42cef47b8f48d6d41f736ee61f5beae40d3f0e50
rgharding <richardharding305@gmail.com>
2023-05-17 05:01:43 -0500

    js101 small problems easy1 #11 book solution added

--- js101/smallProblems/easy1/number11.js
+++ js101/smallProblems/easy1/number11.js
@@ -1,20 +1,36 @@
 // UFT Wrtie function that determines the UTF-16 string value. 
 
-function utf16Value(string) {
-  let indexValuesArray = []
-  for (let index = 0; index < string.length; index += 1) {
-    indexValuesArray.push(string.charCodeAt(index));
-  }
+// function utf16Value(string) {
+//   let indexValuesArray = []
+//   for (let index = 0; index < string.length; index += 1) {
+//     indexValuesArray.push(string.charCodeAt(index));
+//   }
   
-  let result = indexValuesArray.reduce( (acc, cv) => {
-    return acc = acc + cv;
-  }, 0);
-  return result;
+//   let result = indexValuesArray.reduce( (acc, cv) => {
+//     return acc = acc + cv;
+//   }, 0);
+//   return result;
+// }
+
+
+
+// console.log(utf16Value('Four score'));  // 984
+// console.log(utf16Value('Launch School')); //1251
+// console.log(utf16Value('a')); // 97
+// console.log(utf16Value('')); // 0
+
+
+function utf16Value2(string) {
+  let sum = 0;
+
+  for (let i = 0; i < string.length; i += 1) {
+    sum += string.charCodeAt(i);
+  }
+
+  return sum
 }
 
-
-
-console.log(utf16Value('Four score'));  // 984
-console.log(utf16Value('Launch School')); //1251
-console.log(utf16Value('a')); // 97
-console.log(utf16Value('')); // 0
+console.log(utf16Value2('Four score'));  // 984
+console.log(utf16Value2('Launch School')); //1251
+console.log(utf16Value2('a')); // 97
+console.log(utf16Value2('')); // 0

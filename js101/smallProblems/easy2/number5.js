3c466e14080e96500d1ecbc0ddd85c5068de18a1
rgharding <richardharding305@gmail.com>
2023-05-21 21:10:30 -0500

    js101 small problems easy2 #5

---
+++ js101/smallProblems/easy2/number5.js
@@ -0,0 +1,9 @@
+let number1 = prompt('Enter a positive integer');
+let number2 = prompt('Enter a positive integer');
+
+console.log(`number1 + number2 = ${number1 + number2}`);
+console.log(`number1 - number2 = ${number1 - number2}`);
+console.log(`number1 * number2 = ${number1 * number2}`);
+console.log(`number1 / number2 = ${number1 / number2}`);
+console.log(`number1 % number2 = ${number1 % number2}`);
+console.log(`number1 ** number2 = ${Math.pow(number1, number2)}`);

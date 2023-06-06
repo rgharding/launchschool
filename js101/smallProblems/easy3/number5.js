41e100c760c5eb3db985895e1a310937abcc91f6
rgharding <richardharding305@gmail.com>
2023-06-05 21:47:38 -0500

    js101 small problems easy3 #3

---
+++ js101/smallProblems/easy3/number5.js
@@ -0,0 +1,14 @@
+function triangle(num) {
+  let spaces = num - 1;
+  let star = 1;
+
+  while (num > 0) {
+    console.log(`${' '.repeat(spaces)}${'*'.repeat(star)}`);
+    num = num - 1;
+    star = star + 1;
+    spaces = spaces - 1;
+  }
+}
+
+
+triangle(5);

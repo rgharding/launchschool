3cf8d2b4a5b8b0748b7625ba8710b432d2deedc1
rgharding <richardharding305@gmail.com>
2023-06-07 00:42:45 -0500

    js101 small problems easy3 #7

---
+++ js101/smallProblems/easy3/number7.js
@@ -0,0 +1,7 @@
+let noun  = prompt('Enter a noun');
+let verb  = prompt('Enter a verb');
+let adjective  = prompt('Enter an adjective');
+let pronoun  = prompt('Enter a pronoun');
+
+console.log(` A weird madlibs sentence using these words ${noun}${verb}${adjective}${pronoun})
+

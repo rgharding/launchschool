3a69da4baf18a923d508bf2443b35fb8e68d32da
rgharding <richardharding305@gmail.com>
2023-06-08 00:44:10 -0500

    js101 small problems easy3 #7 syntax corrected.

--- js101/smallProblems/easy3/number7.js
+++ js101/smallProblems/easy3/number7.js
@@ -3,5 +3,5 @@ let verb  = prompt('Enter a verb');
 let adjective  = prompt('Enter an adjective');
 let pronoun  = prompt('Enter a pronoun');
 
-console.log(` A weird madlibs sentence using these words ${noun}${verb}${adjective}${pronoun})
+console.log(` A weird madlibs sentence using these words ${noun}${verb}${adjective}${pronoun}`);
 

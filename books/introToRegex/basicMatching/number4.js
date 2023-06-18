06c36b3de144a0e06b9a03f0b36207777551ff0d
rgharding <richardharding305@gmail.com>
2023-06-18 12:01:17 -0500

    deleted number1 from test file stucture

---
+++ books/introToRegex/basicMatching/number4.js
@@ -0,0 +1,8 @@
+let regex = /(banana|apple|orange|strawberry)/;
+
+console.log(regex.test('banana'));
+console.log(regex.test('orange'));
+console.log(regex.test('pineapples'));
+console.log(regex.test('strawberry'));
+console.log(regex.test('raspberry'));
+console.log(regex.test('grappler'));

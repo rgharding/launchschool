dc73ec3da6a945e13c1183e3bc6a98c8855a8d96
rgharding <richardharding305@gmail.com>
2023-07-04 15:26:00 -0500

    js110 lesson1 practice problems 4

---
+++ js110/lesson1/practice_problems/practiceProb4.js
@@ -0,0 +1,8 @@
+// What is the return value of the following statement? Why?
+
+console.log(['ant', 'bear', 'caterpillar'].pop().length);
+
+/*
+This will return the length of the last element of the array 'caterpillar'. This string has the length of 11. The 'pop' method pops the last element of the array and the length string property gives of the length of that value. 
+*/
+

9d94d3ea9609d8a8bc12f7cf138009718addc673
rgh <richardharding305@gmail.com>
2023-07-14 21:30:23 -0500

    example3

---
+++ js110/lesson2/sorting/example3.js
@@ -0,0 +1,23 @@
+/* 
+Sort converts to string and the puts in order of unicode
+Consider the following
+*/
+
+let x = ['arc', 'bat', 'cape', 'ants', 'cap'];
+let y = x.sort();
+
+// What will the following log?
+
+console.log(x) // ['arc', 'bat', 'cape', 'ants', 'cap'];
+console.log(y) // ['ants', 'arc', 'bat', 'cap', 'cape'];
+
+
+/*
+  Correct Answer;
+    - console.log(x) // ['ants', 'arc', 'bat', 'cap', 'cape'];
+    - console.log(y) // ['ants', 'arc', 'bat', 'cap', 'cape'];
+
+
+    Explanation: 
+      Sort is a DESTRUCTIVE method
+*/

9e0a8d4954d068cef6fdc501fb5a6b01bfd225ae
rgh <richardharding305@gmail.com>
2023-07-16 21:40:46 -0500

    example5 sorting

---
+++ js110/lesson2/sorting/example5.js
@@ -0,0 +1,18 @@
+/*
+-JavaScritpt String use UTF-16 code points. Not to be mistaken for ASCII
+-UTF stands for Unicode Transformations Format
+
+- UTF-16 code can be found using Sring.prototype.charCodeAt()
+
+General Rules To Understand
+
+- Uppercase letters come before LowerCase letters
+  'A' = 65  'a' = 97
+
+- Digits and most punctuation come before letters
+
+- There is an extended ASCII table
+
+- All other UTF-16 characters come after the extended ASCII table and have a code point of at least 256
+
+*/

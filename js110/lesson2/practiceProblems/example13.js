aab373db2393a73e61a9e9897401d2c0964e23c6
rgh <richardharding305@gmail.com>
2023-08-20 21:58:38 -0500

    practiceProblems ex13 updated

--- js110/lesson2/practiceProblems/example13.js
+++ js110/lesson2/practiceProblems/example13.js
@@ -10,6 +10,27 @@ let deepCopy = JSON.parse(JSON.stringify(truthiness));
 
 deepCopy.falsy[6] = 666;
 
-console.log(truthiness);
-console.log(deepCopy);
+//console.log(truthiness);
+//console.log(deepCopy);
 
+// The above does not work, it is putting null in place of undefined, and NaN
+
+
+let deepCopyProper = {}
+
+for (let key in truthiness) {
+  deepCopyProper[key] = truthiness[key].slice();
+}
+
+deepCopyProper['falsy'][6] = 666;
+//console.log(deepCopyProper);
+//console.log(truthiness);
+
+
+let deepCopyDestructuring = {};
+
+for (let key in truthiness) {
+  deepCopyDestructuring[key] = [...truthiness[key]];
+}
+
+console.log(deepCopyDestructuring);

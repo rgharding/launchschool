1eff1e95f2414504ece9bb5d3dff79e97e2eba9f
rgh <richardharding305@gmail.com>
2023-09-10 21:20:52 -0500

    smallProblems easy2 number3

---
+++ js110/smallProblems/easy2/number3.js
@@ -0,0 +1,9 @@
+// Write a function that takes 2 arguments and mutiplies them and returns result
+
+
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+console.log(multiply(2, 2) === 4);

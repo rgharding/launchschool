65d7e63443e8e84e742d841f5f33034ad5e8cc11
rgh <richardharding305@gmail.com>
2023-09-21 19:36:04 -0500

    smallProblems easy3 number1 redo with regex

--- js110/smallProblems/easy3/number1.js
+++ js110/smallProblems/easy3/number1.js
@@ -21,8 +21,35 @@ function crunch(inputStr) {
 }
 
 
-console.log(crunch('ddaaiillyy ddoouubbllee'));    // "daily double"
-console.log(crunch('4444abcabccba'));              // "4abcabcba"
-console.log(crunch('ggggggggggggggg'));            // "g"
-console.log(crunch('a'));                          // "a"
-console.log(crunch(''));                           // ""
+//console.log(crunch('ddaaiillyy ddoouubbllee'));    // "daily double"
+//console.log(crunch('4444abcabccba'));              // "4abcabcba"
+//console.log(crunch('ggggggggggggggg'));            // "g"
+//console.log(crunch('a'));                          // "a"
+//console.log(crunch(''));                           // ""
+
+
+function crunch2(inputStr) {
+  let strArr = inputStr.split('');
+  let result = strArr[0];
+
+
+  
+
+  for (let i = 1; i < strArr.length; i += 1) {
+    let nextValue = strArr[i];
+    let regex = new RegExp(`${nextValue}`, 'g')
+    
+    if (regex.test(strArr[i - 1])) {
+      continue;
+    }  else {
+      result += strArr[i]
+    }
+  }
+  return result;
+}
+
+console.log(crunch2('ddaaiillyy ddoouubbllee'));    // "daily double"
+console.log(crunch2('4444abcabccba'));              // "4abcabcba"
+console.log(crunch2('ggggggggggggggg'));            // "g"
+console.log(crunch2('a'));                          // "a"
+console.log(crunch2(''))

b06af573f541cacfbbe867ea9c3552f6a6064816
rgh <richardharding305@gmail.com>
2023-09-26 12:43:55 -0500

    smallProblems easy3 number8

---
+++ js110/smallProblems/easy3/number8.js
@@ -0,0 +1,39 @@
+// Given a string with words spaces and ramdom non-alpha characters, return a string with only alpha charaters (words) with only 1 space between them, replace non-letters , with a space
+
+
+function removeNonAlpha(str) {
+  let regex = /[a-z ]/
+  let lettersAndSpaces = ''
+
+  for (let char of str) {
+    if (regex.test(char)) {
+      lettersAndSpaces += char
+    }
+  }
+
+  return lettersAndSpaces
+}
+
+function removeMultipleSpaces(str) {
+  let correctString = ''
+
+  for (let i = 0; i < str.length; i += 1) {
+    let currentChar = str[i];
+    let nextChar = str[i + 1];
+
+    if (currentChar === ' ' && nextChar === ' ') {
+      continue;
+    } else {
+      correctString += currentChar;
+    }
+  }
+  
+  return correctString;
+}
+
+function cleanUp(str) {
+  let onlyLettersAndSpaces = removeNonAlpha(str);
+  let noMultipleSpaces = removeMultipleSpaces(onlyLettersAndSpaces)
+}
+
+cleanUp("---what's my +*& line");

13bf41a7ef58117bc5391913d83fc55c8b8fc47d
rgh <richardharding305@gmail.com>
2023-09-30 13:21:22 -0500

    smallProblems easy4 number2

---
+++ js110/smallProblems/easy4/number2.js
@@ -0,0 +1,12 @@
+// Write a function that tell if a given string is a palindrme
+
+
+function isPalindrome(str) {
+  let revStr = str.split('').reverse().join('');
+  return str === revStr
+}
+
+console.log(isPalindrome('madam'));               // true
+console.log(isPalindrome('Madam'));               // false (case matters)
+console.log(isPalindrome("madam i'm adam"));      // false (all characters matter)
+console.log(isPalindrome('356653'));              // true

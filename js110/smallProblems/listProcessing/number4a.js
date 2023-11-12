e82c4c70b8f76a73f3a692557dd04f4a4bfb670c
rgh <richardharding305@gmail.com>
2023-11-12 13:12:57 -0600

    smallProblems listProcessing number4a

---
+++ js110/smallProblems/listProcessing/number4a.js
@@ -0,0 +1,15 @@
+function leadingSubstrings(str) {
+  let strArr = str.split('');
+  let subStr = '';
+  
+  let subStringArr = strArr.map((element, currentIndex) => {
+     return subStr += element
+  });
+
+  console.log(subStringArr);
+}
+
+
+leadingSubstrings('abc');      // ["a", "ab", "abc"]
+leadingSubstrings('a');        // ["a"]
+leadingSubstrings('xyzzy');    // ["x", "xy", "xyz", "xyzz", "xyzzy"]//

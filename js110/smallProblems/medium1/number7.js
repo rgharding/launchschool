b53daf4790686512e3522a008a867cdaf827b393
rgh <richardharding305@gmail.com>
2023-12-06 23:59:21 -0600

    smallProblems medium1 number7

---
+++ js110/smallProblems/medium1/number7.js
@@ -0,0 +1,17 @@
+function fibonacci(num) {
+  if (num <= 2) {
+    return 1;
+  }
+
+  return fibonacci(num - 1) + fibonacci(num - 2)
+
+}
+
+
+console.log(fibonacci(1));       // 1
+console.log(fibonacci(2));       // 1
+console.log(fibonacci(3));       // 2
+console.log(fibonacci(4));       // 3
+console.log(fibonacci(5));       // 5
+console.log(fibonacci(12));      // 144
+console.log(fibonacci(20));      // 6765

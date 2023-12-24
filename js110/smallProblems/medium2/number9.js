92f420eb2dc13e6889bc82938087911ea5874f7f
rgh <richardharding305@gmail.com>
2023-12-23 23:48:43 -0600

    smallProblems medium2 number9

---
+++ js110/smallProblems/medium2/number9.js
@@ -0,0 +1,33 @@
+function is_prime(num) {
+  let factors = 0;
+  
+  if (num === 1) {
+    return false;
+  }
+
+  for (let i = num; i > 0; i -= 1) {
+    if (num % i === 0) {
+      factors += 1;
+    }
+  }
+  console.log;(factors)
+  return factors <= 2 ? true : false; 
+}
+
+
+console.log(is_prime(1) === false)            // true
+//console.log(is_prime(2) === true)             // true
+//console.log(is_prime(3) === true)             // true
+//console.log(is_prime(4) === false)            // true
+//console.log(is_prime(5) === true)             // true
+//console.log(is_prime(6) === false)            // true
+//console.log(is_prime(7) === true)             // true
+//console.log(is_prime(8) === false)            // true
+//console.log(is_prime(9) === false)            // true
+//console.log(is_prime(10) === false)           // true
+//console.log(is_prime(23) === true)            // true
+//console.log(is_prime(24) === false)           // true
+//console.log(is_prime(997) === true)           // true
+//console.log(is_prime(998) === false)          // true
+//console.log(is_prime(3_297_061) === true)     // true
+//console.log(is_prime(23_297_061) === false)   // true

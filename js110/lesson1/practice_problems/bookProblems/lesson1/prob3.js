081538e359e7b24bee866e923380993d7557dfc2
rgh <richardharding305@gmail.com>
2024-01-17 19:57:04 -0600

    lesson1 practice problems number3

---
+++ js110/lesson1/practice_problems/bookProblems/lesson1/prob3.js
@@ -0,0 +1,8 @@
+// What will this return
+
+[1, 2, 3].map(num => num * num) 
+
+/*
+This will return a new array with the values of [2, 4, 9]; The return value of this is the current element times the current element. The => syntax with out {} syntax takes the place of explicity witinng 'return'
+
+*/

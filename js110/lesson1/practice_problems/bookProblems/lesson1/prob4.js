67d9a3079ef81ea9a3f8d1fa0b9c6b82002f36fd
rgh <richardharding305@gmail.com>
2024-01-18 20:03:08 -0600

    lesson1 practice problem num4

---
+++ js110/lesson1/practice_problems/bookProblems/lesson1/prob4.js
@@ -0,0 +1,7 @@
+// What is the return value of the following statement? Why?
+
+let x  = ['ant', 'bear', 'caterpillar'].pop().length;
+
+/*
+  This will return 11, the length of caterpillar. The pop method return the last element of the array. The length method returns the value of that element
+*/

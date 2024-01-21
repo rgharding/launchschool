0e29f055c4def5ca973f77d36e1a40d755337c8e
rgh <richardharding305@gmail.com>
2024-01-20 21:38:46 -0600

    lesson1 practiceProblems number6

---
+++ js110/lesson1/practice_problems/bookProblems/lesson1/prob6.js
@@ -0,0 +1,5 @@
+let arr = [1, 2, 3, 4, 5]
+arr.fill(1, 1, 5);
+console.log(arr) // [1, 1, 1, 1, 1, 1]
+
+// Fill is a destructive method. It taske 2 indicies and replaces starting with first indicie and ending before last number indicie

e730bb8c5c1c9c00f815555495e96d510b525d0c
rgh <richardharding305@gmail.com>
2024-02-10 14:23:07 -0600

    number5 redo

---
+++ js110/smallProblems/easy2/number5a.js
@@ -0,0 +1,17 @@
+let sync = require('readline-sync');
+
+let num1 = parseInt(sync.question('Enter first number: '));
+let num2 = parseInt(sync.question('Enter first number: '));
+
+
+console.log(`--> ${num1} + ${num2} = ${num1 + num2}`);
+console.log(`--> ${num1} - ${num2} = ${num1 - num2}`);
+console.log(`--> ${num1} * ${num2} = ${num1 * num2}`);
+console.log(`--> ${num1} / ${num2} = ${num1 / num2}`);
+console.log(`--> ${num1} % ${num2} = ${num1 % num2}`);
+console.log(`--> ${num1} ** ${num2} = ${num1 ** num2}`);
+
+
+    
+  
+

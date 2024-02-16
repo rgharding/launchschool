d19d615a3e2f629f7740690eee33ad1a59ee0402
rgh <richardharding305@gmail.com>
2024-02-16 12:36:05 -0600

    redo number7

---
+++ js110/smallProblems/easy2/number7a.js
@@ -0,0 +1,14 @@
+function xor(param1, param2) {
+  if ((!param1 && param2) || (param1 && !param2)) {
+    return true;
+  } 
+  
+  return false;
+}
+
+console.log(xor(5, 0) === true);          // true
+console.log(xor(false, true) === true);   // true
+console.log(xor(1, 1) === false);         // true
+console.log(xor(true, true) === false);   // true
+console.log(xor(0, false) === false);     // true
+console.log(xor(0, 0) === false);         // true

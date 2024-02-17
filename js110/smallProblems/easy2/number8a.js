2686b2f87f37a198a8605b5831a6d2964c26b3a4
rgh <richardharding305@gmail.com>
2024-02-17 12:44:34 -0600

    redo number8

---
+++ js110/smallProblems/easy2/number8a.js
@@ -0,0 +1,12 @@
+function oddities(arr) {
+  return arr.filter((element, index) => {
+    return index % 2 === 0
+  })
+}
+
+
+console.log(oddities([2, 3, 4, 5, 6])); // logs [2, 4, 6]
+console.log(oddities([1, 2, 3, 4, 5, 6])); // logs [1, 3, 5]
+console.log(oddities(["abc", "def"])); // logs ['abc']
+console.log(oddities([123])); // logs [123]
+console.log(oddities([])); // logs []

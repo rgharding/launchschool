f426cefe9c53734ce7dbffd0b61eccb2b6a20bfa
rgh <richardharding305@gmail.com>
2024-04-04 22:33:59 -0500

    redo number3

--- js110/smallProblems/easy6/number3a.js
+++ js110/smallProblems/easy6/number3a.js
@@ -1,16 +1,11 @@
 function reverseNumber(num) {
-  let numStr = String(num);
-  let revNum = numStr.split('').reverse().join('');
-  let newNum = parseInt(revNum);
+  let revNum = parseInt(String(num).split('').reverse().join(''));
+  return revNum;
 
-  console.log(newNum);
 }
-
 
 reverseNumber(12345);    // 54321
 reverseNumber(12213);    // 31221
 reverseNumber(456);      // 654
 reverseNumber(12000);    // 21 -- Note that leading zeros in the result get dropped!
 reverseNumber(1);        // 1
-
-// Number.prototype.parseInt   removes leading zeros

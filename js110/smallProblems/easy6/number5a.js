636c8ab7dc1c65930939723a79db714135481526
rgh <richardharding305@gmail.com>
2024-04-11 16:07:28 -0500

    number5 supplemental question

--- js110/smallProblems/easy6/number5a.js
+++ js110/smallProblems/easy6/number5a.js
@@ -13,7 +13,21 @@ function swapName(str) {
 function swapName2(str) {
   return str.split(' ').reverse().join(', ')
 }
-console.log(swapName2('Joe Roberts'));    // "Roberts, Joe"
+
+//console.log(swapName2('Joe Roberts'));    // "Roberts, Joe"
+
+
+
+function swapName3(str) {
+  let nameArr = str.split(' ');
+  let firstName = nameArr.shift();
+  let lastName = nameArr.pop();
+  let middleName = nameArr.join(' ');
+
+  return `${lastName}, ${firstName} ${middleName}`
+}
+
+console.log(swapName3('Karl Ragvals'));    // "Ragvals, Karl Oskar Henriksson"
 
 
 

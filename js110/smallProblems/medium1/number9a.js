0de05612fb3590d2dc55b5ee28c44afedd55f620
rgh <richardharding305@gmail.com>
2024-05-16 14:05:45 -0500

    redo number9

--- js110/smallProblems/medium1/number9a.js
+++ js110/smallProblems/medium1/number9a.js
@@ -1,22 +1,16 @@
-let memo = {
-
-};
+let memo = {};
 
 function fibonacci(nth) {
+  
   if (nth <= 2) {
     return 1;
   } else if (memo[nth]) {
-      return memo[nth]
-  } else  {
-      memo[nth] = fibonacci(nth - 1) + fibonacci(nth - 2);
-      return memo[nth];
+    return memo[nth];
+  } else {
+    memo[nth] = fibonacci(nth - 1) + fibonacci(nth - 2);
+    return memo[nth];
   }
-
   
-
-  
-  
-
 }
 
 
@@ -24,6 +18,6 @@ function fibonacci(nth) {
 //console.log(fibonacci(2));       // 1
 //console.log(fibonacci(3));       // 2
 //console.log(fibonacci(4));       // 3
-console.log(fibonacci(5));       // 5
-//console.log(fibonacci(12));      // 144
+//console.log(fibonacci(5));       // 5
+console.log(fibonacci(12));      // 144
 //console.log(fibonacci(20));      // 6765

811a91344635e6108f44769c0f7cfe8c8ef026d8
rgh <richardharding305@gmail.com>
2024-05-24 21:42:50 -0500

    redo number9

---
+++ js110/smallProblems/medium2/number9a.js
@@ -0,0 +1,31 @@
+function is_prime(num) {
+  if (num === 1) return false;
+
+  let multipleArr = []
+
+  for (let i = 1; i <= num; i += 1) {
+    if (num % i === 0) {
+      multipleArr.push(i);
+    }
+  }
+
+  return multipleArr.length <= 2 ? true : false
+}
+
+
+console.log(is_prime(1) === false)            // true
+console.log(is_prime(2) === true)             // true
+console.log(is_prime(3) === true)             // true
+console.log(is_prime(4) === false)            // true
+console.log(is_prime(5) === true)             // true
+console.log(is_prime(6) === false)            // true
+console.log(is_prime(7) === true)             // true
+console.log(is_prime(8) === false)            // true
+console.log(is_prime(9) === false)            // true
+console.log(is_prime(10) === false)           // true
+console.log(is_prime(23) === true)            // true
+console.log(is_prime(24) === false)           // true
+console.log(is_prime(997) === true)           // true
+console.log(is_prime(998) === false)          // true
+console.log(is_prime(3_297_061) === true)     // true
+console.log(is_prime(23_297_061) === false)   // true

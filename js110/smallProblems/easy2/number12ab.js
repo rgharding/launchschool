d38b4e60a010405ad88f27f6e619dcf212e10198
rgh <richardharding305@gmail.com>
2024-07-21 12:55:58 -0500

    redo2 number12

---
+++ js110/smallProblems/easy2/number12ab.js
@@ -0,0 +1,9 @@
+
+function negative(num) {
+  num = Math.abs(num);
+  return num * -1
+}
+
+console.log(negative(5));     // -5
+console.log(negative(-3));    // -3
+console.log(negative(0));     // -0)

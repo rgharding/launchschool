3dac9627a48bd2dc43868229bbda2723f2b14dc9
rgh <richardharding305@gmail.com>
2024-07-27 17:27:33 -0500

    redo2 number5

---
+++ js110/smallProblems/easy3/number5ab.js
@@ -0,0 +1,26 @@
+/*
+[P]roblem: make a mad libs
+
+I/O 
+string
+string
+
+
+[A]lgorithem;
+  -Set variables to the type of word we want, noun, verb ect
+  -Ask user for each variable and set it
+  -Place variable in string story and proper locations 
+  -Return ediited story
+
+Enter a noun: dog
+Enter a verb: walk
+Enter an adjective: blue
+Enter an adverb: quickly
+
+// console output
+
+/*
+Do you walk your blue dog quickly? That's hilarious!
+The blue dog walks quickly over the lazy dog.
+The dog quickly walks up blue Joe's turtle.
+*/

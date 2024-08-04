39426a1553183a269b71aea30593ba27041567dc
rgh <richardharding305@gmail.com>
2024-08-03 19:55:25 -0500

    redo2 number2

---
+++ js110/smallProblems/easy4/number2ab.js
@@ -0,0 +1,10 @@
+function isPalindrome(str) {
+  let revStr = str.split('').reverse().join('');
+
+  return str === revStr;
+}
+
+console.log(isPalindrome('madam'));               // true
+console.log(isPalindrome('Madam'));               // false (case matters)
+console.log(isPalindrome("madam i'm adam"));      // false (all characters matter)
+console.log(isPalindrome('356653'));              // true

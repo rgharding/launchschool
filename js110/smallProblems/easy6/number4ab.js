0996c1c766e66931adb4b5d10a65fcfcbdc19e72
rgh <richardharding305@gmail.com>
2024-09-10 22:28:53 -0500

    redo2 number4

---
+++ js110/smallProblems/easy6/number4ab.js
@@ -0,0 +1,14 @@
+function sequence(num) {
+  let arr = [];
+
+  for (let i = 1; i <= num; i += 1) {
+    arr.push(i)
+  }
+
+  return arr;
+}
+
+
+console.log(sequence(5));    // [1, 2, 3, 4, 5]
+console.log(sequence(3));    // [1, 2, 3]
+console.log(sequence(1));    // [1]

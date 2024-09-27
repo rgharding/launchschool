986fb39452c0085b291d1b743d56d4ba421144dd
rgh <richardharding305@gmail.com>
2024-09-26 22:28:45 -0500

    redo number2

--- js110/smallProblems/easy6/number2a.js
+++ js110/smallProblems/easy6/number2a.js
@@ -1,21 +1,42 @@
 function doubleConsonants(str) {
-  let doubleLetters = ['a', 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'];
+  let regex = /[bcdfghjklmnpqrstvwxyz]/i
+  let newStr = "";
 
-  let newStr = ''
-
-  for (let i = 0; i < str.length; i += 1) {
-    newStr += str[i];
-  
-    if ( doubleLetters.indexOf(str[i].toLowerCase()) >= 0 ) {
-      newStr += str[i];
+  for (let char of str) {
+    if (regex.test(char)) {
+      newStr += char + char   
+    } else {
+      newStr += char
     }
   }
-  
+
   console.log(newStr);
 }
 
 
 //doubleConsonants('String');          // "SSttrrinngg"
-doubleConsonants('Hello-World!');    // "HHellllo-WWorrlldd!"
-doubleConsonants('July 4th');        // "JJullyy 4tthh"
-doubleConsonants('');                // ""
+//doubleConsonants('Hello-World!');    // "HHellllo-WWorrlldd!"
+//doubleConsonants('July 4th');        // "JJullyy 4tthh"
+//doubleConsonants('');                // ""
+
+
+function doubleConsonants2(str) {
+  let CONSTS =  ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'];
+  
+  let newStr = ''
+
+  for (let i = 0; i < str.length; i += 1) {
+    newStr += str[i];
+    
+    if (CONSTS.indexOf(str[i].toLowerCase()) >= 0) {
+      newStr += str[i];
+    }
+  }
+
+  console.log(newStr);
+}
+
+doubleConsonants2('String');          // "SSttrrinngg"
+//doubleConsonants2('Hello-World!');    // "HHellllo-WWorrlldd!"
+//doubleConsonants2('July 4th');        // "JJullyy 4tthh"
+//doubleConsonants2('');                // ""

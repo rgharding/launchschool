f9edea043c9686588c1eabfbf02b83187458c976
rgh <85709223+rgharding@users.noreply.github.com>
2026-07-18 19:24:57 -0500

    number5

---
+++ js120/smallProblems/classesOO/number5.js
@@ -0,0 +1,11 @@
+// create a class with a constructor that has a name paramenter add name to cnsturtor string
+
+class Cat {
+  constructor(name) {
+    this.name = name;
+    console.log(`Hello! My name is Sophie!`);
+  }
+}
+
+
+let kitty = new Cat('Sophie');
